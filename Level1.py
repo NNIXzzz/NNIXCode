ee43d7a0951c364964418cac93da44482fdb97c3
NNIXzzz <nik.iyunin@mail.ru>
2022-12-26 00:26:34 +0400

    v1.1

--- Level1.py
+++ Level1.py
@@ -2,27 +2,32 @@ def level1():
     import random
     import numpy as np
     import sys
+    import string
     
     Scores = 0
     Three = 3
-    diceE = ["adrenaline", "tornado", "burn", "eon", "redbull", "doubleyou", "drive", "gorilla"]
+    Half = 1/2
+    
+    diceE = ["Adrenaline", "Tornado", "Burn", "Eon", "Redbull", "Doubleyou", "Drive", "Gorilla"]
 
     while Scores < 15:
         
-        RDE = random.choice(diceE)
+        RDE = (random.choice(diceE))
+        RDEScore = (Half*len(RDE))
         diceE.remove(RDE)     
         RDE1 = RDE
         RDE1 = RDE1.replace(random.choice(RDE1), '*')
         print(RDE1, end=' - ')
 
         answerdiceE = input()
+        answerdiceE = answerdiceE.capitalize()
         if answerdiceE == RDE:
-            Scores = Scores + Three
+            Scores = Scores + RDEScore
             print("Отгадал!", end=" ")
-            print(("Баллы:"),(Scores),"""
+            print(("Баллы: "),(Scores),"""
                 """, sep="")
     
-        if answerdiceE == "стоп":
+        if answerdiceE == "Стоп":
             sys.exit()
         elif answerdiceE != RDE:
             Scores = np.subtract(Scores, Three)
